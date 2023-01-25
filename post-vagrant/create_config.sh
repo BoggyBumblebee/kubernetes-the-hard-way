20f022fb10bb3d4fb53ea13cde746fb8a90f0fef
Christopher Marsh-Bourdon <cmb@Christophers-Mac-Pro.local>
2023-01-25 13:38:00 +0000

    Added post-Vagrant tasks scripts

---
+++ post-vagrant/create_config.sh
@@ -0,0 +1,97 @@
+LOADBALANCER=$(dig +short loadbalancer)
+
+## Generate a kubeconfig file for the kube-proxy service
+{
+  kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=/var/lib/kubernetes/pki/ca.crt \
+    --server=https://${LOADBALANCER}:6443 \
+    --kubeconfig=kube-proxy.kubeconfig
+
+  kubectl config set-credentials system:kube-proxy \
+    --client-certificate=/var/lib/kubernetes/pki/kube-proxy.crt \
+    --client-key=/var/lib/kubernetes/pki/kube-proxy.key \
+    --kubeconfig=kube-proxy.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:kube-proxy \
+    --kubeconfig=kube-proxy.kubeconfig
+
+  kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+}
+
+## Generate a kubeconfig file for the kube-controller-manager service
+{
+  kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=/var/lib/kubernetes/pki/ca.crt \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+  kubectl config set-credentials system:kube-controller-manager \
+    --client-certificate=/var/lib/kubernetes/pki/kube-controller-manager.crt \
+    --client-key=/var/lib/kubernetes/pki/kube-controller-manager.key \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:kube-controller-manager \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+  kubectl config use-context default --kubeconfig=kube-controller-manager.kubeconfig
+}
+
+## Generate a kubeconfig file for the kube-scheduler service
+{
+  kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=/var/lib/kubernetes/pki/ca.crt \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+  kubectl config set-credentials system:kube-scheduler \
+    --client-certificate=/var/lib/kubernetes/pki/kube-scheduler.crt \
+    --client-key=/var/lib/kubernetes/pki/kube-scheduler.key \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:kube-scheduler \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+  kubectl config use-context default --kubeconfig=kube-scheduler.kubeconfig
+}
+
+## Generate a kubeconfig file for the admin user
+{
+  kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca.crt \
+    --embed-certs=true \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=admin.kubeconfig
+
+  kubectl config set-credentials admin \
+    --client-certificate=admin.crt \
+    --client-key=admin.key \
+    --embed-certs=true \
+    --kubeconfig=admin.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=admin \
+    --kubeconfig=admin.kubeconfig
+
+  kubectl config use-context default --kubeconfig=admin.kubeconfig
+}
+
+## Copy the appropriate kube-proxy kubeconfig files to each worker instance
+{
+  for instance in worker-1 worker-2; do
+    scp kube-proxy.kubeconfig ${instance}:~/
+  done
+}
+
+## Copy the appropriate admin.kubeconfig, kube-controller-manager and kube-scheduler kubeconfig files to each controller instance
+{
+  for instance in master-1 master-2; do
+    scp admin.kubeconfig kube-controller-manager.kubeconfig kube-scheduler.kubeconfig ${instance}:~/
+  done
+}
